a9f81e284e6be049a0030885d863db3055e3596c
Amanda <chang.ching@husky.neu.edu>
2018-10-06 17:46:38 -0700

    Assignment 4

---
+++ assignments/Assignment4.java
@@ -0,0 +1,57 @@
+public class Assignment4 {
+	
+	/**
+	 * The deadline of assignment4 is 10/12/2018 23:59 PST.
+	 * Please feel free to contact Zane and Amanda for any questions.
+	 */
+	
+	/*
+	 * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+	 * s = "leetcode" return 0. 
+	 * s = "loveleetcode" return 2.
+	 */
+    public int firstUniqChar(String s) {
+    		//TODO
+     }
+    
+    /*
+     *Given a non-negative integer num, repeatedly add all its digits until the result has only one digit.
+     * Input: 38 Output: 2
+     * Explanation: The process is like: 3 + 8 = 11, 1 + 1 = 2. Since 2 has only one digit, return it.
+     */
+    public int addDigits(int n) {
+    		//TODO
+    }
+    
+    /*
+     *  Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+     *  Input: [0,1,0,3,12] Output: [1,3,12,0,0]
+     */
+    public void moveZeroes(int[] nums) {
+    		//TODO
+     }
+	
+    /*
+     * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+     * Input: "babad" Output: "bab" Note: "aba" is also a valid answer.
+     */
+	public String longestPalindrome(String s) {
+		//TODO
+    }
+	
+    /*
+     * You are given an n x n 2D matrix representing an image. Rotate the image by 90 degrees (clockwise).
+     * Given input matrix = [ [1,2,3],
+  							 [4,5,6],
+  							 [7,8,9] ],
+
+	* rotate the input matrix in-place such that it becomes: [  [7,4,1],
+  															  [8,5,2],
+  															  [9,6,3] ],
+     */
+    public int[][] rotate(int[][] matrix) {
+    		//TODO
+   }
+    
+}
+
