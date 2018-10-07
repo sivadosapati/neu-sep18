6adf485cd760cdb6651d836dea4e52846804d8dd
Amanda <chang.ching@husky.neu.edu>
2018-10-06 17:47:43 -0700

    Update Assignment4.java

--- assignments/Assignment4.java
+++ assignments/Assignment4.java
@@ -42,12 +42,11 @@ public class Assignment4 {
     /*
      * You are given an n x n 2D matrix representing an image. Rotate the image by 90 degrees (clockwise).
      * Given input matrix = [ [1,2,3],
-  							 [4,5,6],
-  							 [7,8,9] ],
-
-	* rotate the input matrix in-place such that it becomes: [  [7,4,1],
-  															  [8,5,2],
-  															  [9,6,3] ],
+  			      [4,5,6],
+  			      [7,8,9] ],
+     * rotate the input matrix in-place such that it becomes: [  [7,4,1],
+  								 [8,5,2],
+  								  [9,6,3] ],
      */
     public int[][] rotate(int[][] matrix) {
     		//TODO
